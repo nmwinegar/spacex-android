bfbe54649d21374e1c24bb3764e9dcefd1ccda15
Nick Winegar <nmwinegar@gmail.com>
2018-03-14 17:52:38 -0500

    Additional launch details

--- app/src/main/java/com/nickwinegar/spacexdemo/model/Launch.java
+++ app/src/main/java/com/nickwinegar/spacexdemo/model/Launch.java
@@ -5,10 +5,13 @@ import com.google.gson.annotations.SerializedName;
 public class Launch {
     @SerializedName("flight_number")
     public int flightNumber;
+    public String details;
     public Rocket rocket;
     public LaunchLinks links;
     @SerializedName("launch_date_unix")
     public long launchDateTimestamp;
+    @SerializedName("launch_success")
+    public boolean launchSuccess;
 
     public class LaunchLinks {
         @SerializedName("mission_patch")
@@ -17,5 +20,12 @@ public class Launch {
         public String videoUrl;
 
         public String highlightImageUrl;
+    }
+
+    public class LaunchSite {
+        @SerializedName("site_id")
+        public String id;
+        @SerializedName("site_name_long")
+        public String name;
     }
 }
